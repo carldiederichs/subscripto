a15960563ffc80b1046fecf2760b86dfeaf89161
Niccolas Farani <farani.niccolas96@gmail.com>
2020-03-03 16:07:31 +0000

    did login/signup form design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,15 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
