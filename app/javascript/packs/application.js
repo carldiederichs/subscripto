01c423dbb4160e9ae5545b6ac940465cd3565f6d
Carl Diederichs <carl-diederichs@t-online.de>
2020-03-04 02:14:55 +0000

    changed navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ require("channels")
 
 import "bootstrap";
 
+//start of js code for signup page
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -15,3 +16,17 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
+//end of js code for signup page
+
+
+//start of js code for navbar on homepage
+let underlineMenuItems = document.querySelectorAll(".underline-menu li");
+underlineMenuItems[0].classList.add("active");
+underlineMenuItems.forEach(item => {
+  item.addEventListener("click", () => {
+    underlineMenuItems.forEach(item => item.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+//end of js code for navbar on homepage
+
