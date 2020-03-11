51007b75d0298bb08e8b11754efa86c6f96aa01c
Louis Choo-Choy <lchoochoy@gmail.com>
2020-03-11 11:30:59 +0000

    done validations and seed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,10 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :company
 
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :renewal_date, presence: true
+
   include PgSearch::Model
   pg_search_scope :search_by_title, against: :title
 end
