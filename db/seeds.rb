c182e39271be0f53e16678687f4b330bce2a77cf
Louis Choo-Choy <lchoochoy@gmail.com>
2020-07-06 11:35:40 +0100

    made slight alterations to seed data to make it more up to date

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ user = User.create(email: "zach@gmail.com", password: "123456")
 
 company1 = Company.create(name: "Netflix")
 company2 = Company.create(name: "Spotify")
-company3 = Company.create(name: "Hulu")
+company3 = Company.create(name: "Soundcloud")
 company4 = Company.create(name: "Apple")
 company5 = Company.create(name: "Audible")
 company6 = Company.create(name: "YouTube")
@@ -23,12 +23,12 @@ company7 = Company.create(name: "GitHub")
 
 # company = Company.all.last
 puts 'Creating subscriptions...'
-Subscription.create(user: user, company: company1, title: 'Netflix', price: 8.99, renewal_date: Date.parse("2020/03/13"), creation_date: Date.today)
-Subscription.create(user: user, company: company2, title: 'Spotify', price: 9.99, renewal_date: Date.parse("2020/03/14"), creation_date: Date.today)
-Subscription.create(user: user, company: company3, title: 'Hulu', price: 5.99, renewal_date: Date.parse("2020/03/21"), creation_date: Date.today)
-Subscription.create(user: user, company: company4, title: 'Apple Music', price: 9.99, renewal_date: Date.parse("2020/04/1"), creation_date: Date.today)
-Subscription.create(user: user, company: company5, title: 'Audible', price: 8.99, renewal_date: Date.parse("2020/04/1"), creation_date: Date.today)
-Subscription.create(user: user, company: company6, title: 'YouTube Premium', price: 4.99, renewal_date: Date.parse("2020/04/1"), creation_date: Date.today)
-Subscription.create(user: user, company: company7, title: 'Github', price: 10.99, renewal_date: Date.parse("2020/04/3"), creation_date: Date.today)
+Subscription.create(user: user, company: company1, title: 'Netflix', price: 8.99, renewal_date: Date.parse("2020/07/13"), creation_date: Date.today)
+Subscription.create(user: user, company: company2, title: 'Spotify', price: 9.99, renewal_date: Date.parse("2020/07/14"), creation_date: Date.today)
+Subscription.create(user: user, company: company3, title: 'Soundcloud', price: 5.99, renewal_date: Date.parse("2020/07/21"), creation_date: Date.today)
+Subscription.create(user: user, company: company4, title: 'Apple Music', price: 9.99, renewal_date: Date.parse("2020/08/1"), creation_date: Date.today)
+Subscription.create(user: user, company: company5, title: 'Audible', price: 8.99, renewal_date: Date.parse("2020/08/1"), creation_date: Date.today)
+Subscription.create(user: user, company: company6, title: 'YouTube Premium', price: 4.99, renewal_date: Date.parse("2020/08/1"), creation_date: Date.today)
+Subscription.create(user: user, company: company7, title: 'Github', price: 10.99, renewal_date: Date.parse("2020/08/3"), creation_date: Date.today)
 puts 'Seeding done'
 
